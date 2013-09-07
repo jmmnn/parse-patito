cb68e0ff06351034ae6ab36d7e0008bdce80ad65
Jorge Mtz <jmmnn@yahoo.com>
2013-09-07 14:34:33 -0400

    1st draft

---
+++ referenceparser.py
@@ -0,0 +1,24 @@
+#crawl and index using Solr
+
+#get from Solr a list of all the document IDs in the collection. Variable is type set: solr_ids
+#open each doc though Solr rest api 
+#parse each document to find its own document symbol. Variable type string: doc_symbol
+#create a dictionary to keep pairs [solr_id : doc_symbol]
+
+
+
+
+
+
+
+
+#TODO later
+
+
+#parse the body of each document to find citations to other documents e.g. A/RES/...
+
+# write a new document keeping the citations from each document: 
+# e.g. docsymbol X [docsymbol Y, Doc Z] , doc A [doc X, doc B]
+
+# 
+
