9bf6f27d4d5e55348475d658fe4f84863525e434
Jorge Mtz <jmmnn@yahoo.com>
2013-09-07 15:36:49 -0400

    saving python meetup

--- referenceparser.py
+++ referenceparser.py
@@ -1,6 +1,17 @@
-#crawl and index using Solr
-
+#crawl and index using Solr - done
 #get from Solr a list of all the document IDs in the collection. Variable is type set: solr_ids
+import urllib
+import re
+
+query_url= "http://127.0.0.1:8888/solr/patito-files/select?q=*%3A*&start=0&rows=50&fl=id&wt=csv&indent=true" #change the rows=  to get a large dataset
+query_result = urllib.urlopen(query_url)
+query_result_string = query_result.read()
+#print query_result_string  #uncomment this to see the solr_ids
+
+#keep only domains of interest e.g. un.org
+solr_ids_list = re.findall(r'\w*.pdf', query_result_string)
+print solr_ids_list
+
 #open each doc though Solr rest api 
 #parse each document to find its own document symbol. Variable type string: doc_symbol
 #create a dictionary to keep pairs [solr_id : doc_symbol]
@@ -13,12 +24,8 @@
 
 
 #TODO later
-
-
 #parse the body of each document to find citations to other documents e.g. A/RES/...
-
 # write a new document keeping the citations from each document: 
 # e.g. docsymbol X [docsymbol Y, Doc Z] , doc A [doc X, doc B]
-
-# 
+ 
 
